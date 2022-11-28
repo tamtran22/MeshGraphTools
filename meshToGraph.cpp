6b031ac4dbb85e9e944fb280e532a37813398460
tamtran22 <hocsinhchuyentin@gmail.com>
2022-11-28 10:19:27 +0900

    test 221128

--- meshToGraph.cpp
+++ meshToGraph.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "mesh.h"
 #include <vector>
+#include <string>
 
 using namespace std;
 
-int main(){
-    Mesh Th;
-    Th.fromTecplot("first-396.dat");
-    Th.toTecplot("output.dat");
+int main(int argc, char *argv[]){
+    string fileName;
+    if (argc == 0) {
+        fileName = "default.dat";
+    } else {
+        fileName = argv[0];
+    }
+    cout<<fileName<<endl;
+    // Mesh Th;
+    // Th.fromTecplot("first-396.dat");
+    // Th.toTecplot("output.dat");
     return 0;
 }
